c96c1cb13ebadad6b12717a263f904da3bb53bb2
Anas Yar Khan <yarkhananas25@gmail.com>
2025-07-30 04:45:28 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+This is test.py and this is a multiline comment
+'''
+#This is single line comment
+
+from package.maths import *
+from package.subpackage.mul import *
+
+print(addition(3,6))
+print(subtraction(13,6))
+print(multiply(3,5))
